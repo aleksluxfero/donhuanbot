c00224bbf8a35f898f0ecf29802e872bac6ceeea
Lux <aleks.luxfero@gmail.com>
2025-05-12 10:26:45 +0500

    Настройка eslint, prettier

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["grammy"],
+  functions: {
+    maxDuration: 60, // Увеличиваем до 30 секунд для обработки запросов к внешнему API
+  },
 };
 
 export default nextConfig;
