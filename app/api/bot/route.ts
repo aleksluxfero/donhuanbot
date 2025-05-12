150bc70485ad226d3b4a9634880775aba7f165a7
Lux <aleks.luxfero@gmail.com>
2025-05-12 10:52:12 +0500

    test

--- app/api/bot/route.ts
+++ app/api/bot/route.ts
@@ -1,6 +1,14 @@
 import { handleWebhook } from "@/lib/bot";
 import { NextRequest } from "next/server";
 
+export async function GET(request: NextRequest) {
+  console.log(request);
+  return new Response("Эндпоинт Telegram API работает!", {
+    status: 200,
+    headers: { "Content-Type": "text/plain" },
+  });
+}
+
 export async function POST(request: NextRequest) {
   try {
     // Передаем запрос в обработчик вебхуков grammY
